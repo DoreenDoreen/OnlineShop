33fd38f4b44096825121ca75f59000a5496ff3fb
LinLinLin <77956068+DoreenDoreen@users.noreply.github.com>
2021-06-16 00:17:50 -0600

    update ProductService and ProductController

---
+++ onlineShop/src/main/java/onlineShop/controller/ProductController.java
@@ -0,0 +1,50 @@
+package onlineShop.controller;
+
+
+import onlineShop.entity.Product;
+import onlineShop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/getAllProducts", method = RequestMethod.GET)
+    public ModelAndView getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ModelAndView("productList", "products", products);
+    }
+
+    @RequestMapping(value = "/getProductById/{productId}", method = RequestMethod.GET)
+    public ModelAndView getProductById(@PathVariable(value = "productId") int productId) {
+        Product product = productService.getProductById(productId);
+        return new ModelAndView("productPage", "product", product);
+    }
+
+    public String addProduct(@ModelAttribute Product product) {
+
+    }
+
+    public String deleteProduct() {
+
+    }
+
+    public ModelAndView getEditForm() {
+
+    }
+
+    public String editProduct() {
+
+    }
+
+}
