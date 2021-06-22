85886ad3a4fede80b16d62d706826bfa6b89881f
LinLinLin <77956068+DoreenDoreen@users.noreply.github.com>
2021-06-21 19:36:04 -0600

    update CartItemDao, CartItemController, CartItemService

--- onlineShop/src/main/java/onlineShop/controller/CartItemController.java
+++ onlineShop/src/main/java/onlineShop/controller/CartItemController.java
@@ -1,4 +1,31 @@
 package onlineShop.controller;
 
+import onlineShop.dao.CartItemDao;
+import onlineShop.entity.Cart;
+import onlineShop.entity.CartItem;
+import onlineShop.service.CartItemService;
+import onlineShop.service.CustomerService;
+import onlineShop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
 public class CartItemController {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    public ProductService productService;
+
+    
+
+
 }
