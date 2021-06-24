f4287efa57ee5032b928b521bc6f51f8f340c330
LinLinLin <77956068+DoreenDoreen@users.noreply.github.com>
2021-06-23 23:21:17 -0600

    update

--- onlineShop/src/main/java/onlineShop/service/ProductService.java
+++ onlineShop/src/main/java/onlineShop/service/ProductService.java
@@ -1,5 +1,6 @@
 package onlineShop.service;
 
+import onlineShop.dao.ProductDao;
 import onlineShop.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
